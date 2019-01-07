ebd4cd8d708a2b826497f12964e9b1d843a0625e
shenzhe <shenzhe163@gmail.com>
2019-01-07 17:36:29 +0800

    添加fswatch代码，无痛热更

--- application/config/router.php
+++ application/config/router.php
@@ -5,7 +5,7 @@ return [
         $r->addRoute('GET', '/user/{uid:\d+}', 'controller\Index@user');
         $r->get('/add', ['controller\Index', 'add']);
         $r->get('/test', function () {
-            return "i am test 55566";
+            return "i am test";
         });
         $r->post('/post', function () {
             return "must post method";
